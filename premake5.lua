9f351d91d4556c44f291a33b47c4453798baf533
heintjepeintje <heintjepeintje06@gmail.com>
2024-10-30 11:04:07 +0100

    Added premake support

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	
+	files {
+		"./imgui_demo.cpp",
+		"./imgui_draw.cpp",
+		"./imgui_widgets.cpp",
+		"./imgui.cpp",
+		"./backends/imgui_impl_win32.cpp",
+		"./backends/imgui_impl_opengl3.cpp"
+	}
+
+	includedirs {
+		"./backends",
+		"."
+	}
+	
+	targetdir "./Binaries/Output/%{cfg.buildcfg}"
+	objdir "./Binaries/Intermediate/%{cfg.buildcfg}"
+
+	filter "configurations:Debug"
+      defines { "_DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
